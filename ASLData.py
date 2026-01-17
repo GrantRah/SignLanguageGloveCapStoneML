c523649069a1b51242dcccde1a420182f7426aea
Paul <ppowell6@unl.edu>
2026-01-16 21:18:29 -0600

    implemented a while loop into the ASLData script that writes in the data stored within variables for up to 20 entries

--- ASLData.py
+++ ASLData.py
@@ -1,7 +1,14 @@
 import csv
+index,middle,ring,pinky,thumb,sign,=0,0,0,0,0,"a"
+i = 0
+
 # This script makes a CSV file to store ASL finger position data
 # First it creates the initial headers of the row 
 # Then using a loop it will add new rows that have the data for each finger along with the actual sign
 with open('ASLData.csv','w+') as f:
     writer=csv.writer(f)
     writer.writerow(['index','middle','ring','pinky','thumb','sign'])
+    while i < 20:
+        writer.writerow([index,middle,ring,pinky,thumb,sign])
+        i+=1
+        index+=1
