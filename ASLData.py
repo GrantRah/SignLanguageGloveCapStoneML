4d400df798d2bd0a6a440a6e2a8e69a9bd61b096
Paul <ppowell6@unl.edu>
2026-01-18 13:51:23 -0600

    updated the ASL data to append new data instead of just overwriting the old data so now it can better be used to collect data for each sign in stages instead of doing it all at once

--- ASLData.py
+++ ASLData.py
@@ -5,6 +5,7 @@ i = 0
 # This script makes a CSV file to store ASL finger position data
 # First it creates the initial headers of the row 
 # Then using a loop it will add new rows that have the data for each finger along with the actual sign
+# Running this portion of the code will overwrite any existing ASLData.csv file
 with open('ASLData.csv','w+') as f:
     writer=csv.writer(f)
     writer.writerow(['index','middle','ring','pinky','thumb','sign'])
@@ -12,3 +13,12 @@ with open('ASLData.csv','w+') as f:
         writer.writerow([index,middle,ring,pinky,thumb,sign])
         i+=1
         index+=1
+
+#The following code appends new data to the CSV file
+# Each time it is run it will add one new row to the existing ASLData.csv file
+with open('ASLData.csv','a') as f:
+    writer=csv.writer(f)
+    
+    writer.writerow([2,middle,ring,pinky,thumb,sign])
+        
+        
